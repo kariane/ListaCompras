93d0fc16a6f76a8f416aed2b82546da14c29c717
kariane.ccamorim <karianecoura123@gmail.com>
2022-05-17 22:24:23 -0300

    aula 22/05/17

--- app/src/main/java/com/example/listacompras/MainActivity.kt
+++ app/src/main/java/com/example/listacompras/MainActivity.kt
@@ -3,17 +3,45 @@ package com.example.listacompras
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.example.listacompras.databinding.ActivityMainBinding
 import com.firebase.ui.auth.AuthUI
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
+    lateinit var database: DatabaseReference
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         tratarLogin()
 
+        binding.fab.setOnClickListener {
+            novoItem()
+        }
+    }
+    fun novoItem(){
+        val caixaTexto = EditText(this)
+        caixaTexto.hint = "Nome do Item"
+
+        AlertDialog.Builder(this)
+            .setTitle("Novo Item")
+            .setView(caixaTexto)
+            .setPositiveButton("adicionar") { dialog, button ->
+                val prod = Produto(nome = caixaTexto.text.toString())
+                val novoNo = database.child("produtos").push()
+                novoNo.setValue(prod)
+            }
+            .create()
+            .show()
     }
 
     fun tratarLogin(){
@@ -38,9 +66,17 @@ class MainActivity : AppCompatActivity() {
 
         if(requestCode == 1 && resultCode == RESULT_OK){
             Toast.makeText(this, "Autenticado", Toast.LENGTH_LONG).show()
+            configuraBase()
         }
         else {
             finishAffinity()
+        }
+    }
+    fun configuraBase() {
+        val usuario = FirebaseAuth.getInstance().currentUser
+
+        if (usuario != null) {
+            database = FirebaseDatabase.getInstance().reference.child(usuario.uid)
         }
     }
 }
